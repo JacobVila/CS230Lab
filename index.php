5a0f029fd1a17910af491fca63e9febe8ea0f231
JacobVila <jv00002@mix.wvu.edu>
2021-02-03 14:21:22 -0500

    Lab2

---
+++ index.php
@@ -0,0 +1,53 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us </h1>
+        <p>"Big Lakes is a quality fish dealer. We hope to meet all your fishy needs!"</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/JohnPeterson.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>John Peterson</p>
+                    <p>JohnPeterson@Fmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/JoelTorrez.jpeg">
+                <div class="container">
+                    <h2>CFO</h2>
+                    <p>Joel Torrez</p>
+                    <p>JTorrez32@Fmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/SusanSorrensonn.jpeg">
+                <div class="container">
+                    <h2>CMO</h2>
+                    <p>Susan Sorrensonn</p>
+                    <p>SusanS@Fmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/PeteParker.jpeg">
+                <div class="container">
+                    <h2>Fish Specialist</h2>
+                    <p>Pete Parker</p>
+                    <p>PP76@Fmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
